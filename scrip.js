179df883eee3998d738be029e8b29d256ce0ceb8
Ohsnamiboy <obamasun2966@gmail.com>
2023-11-25 02:24:53 +0900

    test

--- scrip.js
+++ scrip.js
@@ -1,7 +1,12 @@
-const dayOfWeek = [mon, tue, wed, thu, fri, sat, sun]  ;
-const nonsense = [1, 2, "hello", false, null, true, undefined, "bama"];
+function foo(arg) {
+  arg();
+}
 
-console.log (dayOfWeek, nonsense);
+foo(() => {
+  console.log(1);
+});
 
-
- 
+// 1. foo 실행
+// 2. foo 함수는 arg 함수를 실행. (변수 아무넣고 안넣고)
+// 3. bar 함수가 실행 됨 (변수에 아무넣고 안들어가고)
+// 4. bar 가 실행되어 "bar" 가 출력됨
