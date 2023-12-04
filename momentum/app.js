568549a05ffe6b78a90be9d00ff58d015119a717
Ohsnamiboy <obamasun2966@gmail.com>
2023-12-04 19:13:20 +0900

    2-7

---
+++ momentum/app.js
@@ -0,0 +1,9 @@
+const player = {
+  name: "nico",
+  sayHello: function (otherPersonsName) {
+    console.log("hello" + otherPersonsName + " nice to meet you");
+  },
+};
+
+player.sayHello("lynn");
+player.sayHello("nico");
