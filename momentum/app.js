d05258e1490ed2fb745fdb3116b0bc55532589bc
Ohsnamiboy <obamasun2966@gmail.com>
2023-12-05 14:26:04 +0900

    3-6

--- momentum/app.js
+++ momentum/app.js
@@ -1,36 +1,14 @@
 const h1 = document.querySelector("div.hello:first-child h1");
 
 function handleTitleClick() {
-  h1.style.color = "blue";
+  const currentColor = h1.style.color;
+  let newColor;
+  if (currentColor === "blue") {
+    newColor = "tomato";
+  } else {
+    newColor = "blue";
+  }
+  h1.style.color = newColor;
 }
 
-function handleMouseEnter() {
-  h1.innerText = "mouse is here!";
-}
-
-function handleMouseleave() {
-  h1.innerText = "mouse is gone!";
-}
-
-function handleleWindowResize() {
-  document.body.style.backgroundColor = "tomato";
-}
-
-function handleWindowCopy() {
-  alert("copier!");
-}
-function handleWindowOffline() {
-  alert("SOS no WIFI");
-}
-
-function handleWindowOnline() {
-  alert("ALL GOOOD!");
-}
-h1.onclick = handleTitleClick;
-h1.onmouseenter = handleMouseEnter;
-h1.addEventListener("mouseleave", handleMouseleave);
-
-window.addEventListener("resize", handleleWindowResize);
-window.addEventListener("copy", handleWindowCopy);
-window.addEventListener("offline", handleWindowOffline);
-window.addEventListener("online", handleWindowOnline);
+h1.addEventListener("click", handleTitleClick);
