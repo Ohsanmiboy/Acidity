aaae1cff9fc282a9e996162d68be6daa59eb2a40
Ohsnamiboy <obamasun2966@gmail.com>
2023-12-06 02:21:48 +0900

    4-4

--- momentum/app.js
+++ momentum/app.js
@@ -1,14 +1,21 @@
 const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
-const link = document.querySelector("a");
+const greeting = document.querySelector("#greeting");
+const HIDDEN_CLASSNAME = "hidden";
+// const link = document.querySelector("a");
+
 function onLoginsubmit(event) {
   event.preventDefault();
-  console.log(event);
+  loginForm.classList.add(HIDDEN_CLASSNAME);
+  const username = loginInput.value;
+  greeting.innerText = `hello ${username}`;
+  greeting.classList.remove(HIDDEN_CLASSNAME);
 }
+
 function handleLinkClick(event) {
   event.preventDefault();
   console.log(event);
 }
 loginForm.addEventListener("submit", onLoginsubmit);
 
-link.addEventListener("click", handleLinkClick);
+// link.addEventListener("click", handleLinkClick);
