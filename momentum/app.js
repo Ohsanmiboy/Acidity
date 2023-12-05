b567224bf652f64d40da7df4881b5354cd6d5076
Ohsnamiboy <obamasun2966@gmail.com>
2023-12-05 17:54:53 +0900

    4-2

--- momentum/app.js
+++ momentum/app.js
@@ -1,9 +1,11 @@
+const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
-const loginButton = document.querySelector("#login-form button");
 
-function onLoginBtnClick() {
-  const username = loginInput.value;
-  console.log(username);
+function onLoginsubmit(tomato) {
+  tomato.preventDefault();
+  console.log(tomato);
 }
 
-loginButton.addEventListener("click", onLoginBtnClick);
+loginForm.addEventListener("submit", onLoginsubmit);
+
+onLoginsubmit();
