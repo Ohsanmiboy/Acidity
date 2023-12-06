7bdd479640b1fcdda1992d822cf440eb1220796f
Ohsnamiboy <obamasun2966@gmail.com>
2023-12-06 10:31:18 +0900

    4-6

--- momentum/app.js
+++ momentum/app.js
@@ -2,21 +2,31 @@ const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
 const greeting = document.querySelector("#greeting");
 const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "username";
 // const link = document.querySelector("a");
 
 function onLoginsubmit(event) {
   event.preventDefault();
   loginForm.classList.add(HIDDEN_CLASSNAME);
   const username = loginInput.value;
-  localStorage.setItem("username", username);
-  greeting.innerText = `hello ${username}`;
+  localStorage.setItem(USERNAME_KEY, username);
+  paintGreetings(username);
+}
+
+loginForm.addEventListener("submit", onLoginsubmit);
+
+function paintGreetings(username) {
+  greeting.innerText = `Hello ${username}`;
   greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
-function handleLinkClick(event) {
-  event.preventDefault();
-  console.log(event);
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+
+if (savedUsername === null) {
+  loginForm.classList.remove(HIDDEN_CLASSNAME);
+  loginForm.addEventListener("submit", onLoginsubmit);
+} else {
+  paintGreetings(savedUsername);
 }
-loginForm.addEventListener("submit", onLoginsubmit);
 
 // link.addEventListener("click", handleLinkClick);
