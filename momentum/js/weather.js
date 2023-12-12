82e5298b48d2c93d499e4a5c310c1914085d4160
Ohsnamiboy <obamasun2966@gmail.com>
2023-12-12 14:58:59 +0900

    8-2

--- momentum/js/weather.js
+++ momentum/js/weather.js
@@ -3,7 +3,6 @@ const API_KEY = "b510c93096ecbe18ea899032322843b0";
 function onGeoOk(position) {
   const lat = position.coords.latitude;
   const lng = position.coords.longitude;
-  console.log("You live in", lat, lng);
   const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${API_KEY}&units=metric`;
   fetch(url)
     .then((response) => response.json())
