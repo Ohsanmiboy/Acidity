82486941a7ddff5cbfa1a2463d4cac745a0d6d9b
Ohsnamiboy <obamasun2966@gmail.com>
2023-12-19 14:38:55 +0900

    greeting.js추가,input태가운데옮기기실패

--- momentum/js/greeting.js
+++ momentum/js/greeting.js
@@ -3,25 +3,36 @@ const loginInput = document.querySelector("#login-form input");
 const greeting = document.querySelector("#greeting");
 const HIDDEN_CLASSNAME = "hidden";
 const USERNAME_KEY = "username";
+const logoutKey = document.querySelector("#log-out");
 
-const savedUsername = localStorage.getItem(USERNAME_KEY);
-
-if (savedUsername === null) {
-  loginForm.classList.remove(HIDDEN_CLASSNAME);
-  loginForm.addEventListener("submit", onLoginsubmit);
-} else {
-  paintGreetings();
+function onLoginsubmit(event) {
+  loginForm.addEventListener("submit", (event) => {
+    event.preventDefault();
+    loginForm.classList.add(HIDDEN_CLASSNAME);
+    localStorage.setItem(USERNAME_KEY, loginInput.value);
+    paintGreetings();
+  });
 }
-
-loginForm.addEventListener("submit", (event) => {
-  event.preventDefault();
-  loginForm.classList.add(HIDDEN_CLASSNAME);
-  localStorage.setItem(USERNAME_KEY, loginInput.value);
-  paintGreetings();
-});
 
 function paintGreetings() {
   const username = localStorage.getItem(USERNAME_KEY);
   greeting.innerText = `Hello ${username}`;
   greeting.classList.remove(HIDDEN_CLASSNAME);
 }
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+
+if (savedUsername == null) {
+  loginForm.classList.remove(HIDDEN_CLASSNAME);
+  loginForm.addEventListener("submit", onLoginsubmit);
+} else {
+  paintGreetings();
+}
+
+logoutKey.addEventListener("submit", (event) => {
+  if (saveUsername === null) {
+    logoutKey.cassList.remove(HEDDEN_CLASSNAME);
+    localStorage.removeItem(USERNAME_KEY, loginInput.value);
+  } else {
+    logoutKey.cassList.add(HEDDEN_CLASSNAME);
+  }
+});
